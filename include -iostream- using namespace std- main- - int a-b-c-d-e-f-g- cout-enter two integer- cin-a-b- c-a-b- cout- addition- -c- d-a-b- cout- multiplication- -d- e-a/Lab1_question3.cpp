f5adff800bbdcf611c38f6763a7ca14b4ba84782
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-25 17:09:41 +0530

    Create Lab1_question3.cpp

---
+++ include -iostream- using namespace std- main- - int a-b-c-d-e-f-g- cout-enter two integer- cin-a-b- c-a-b- cout- addition- -c- d-a-b- cout- multiplication- -d- e-a/Lab1_question3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+main() {
+int a,b,c,d,e,f,g;
+cout<<"enter two integer";
+cin>>a>>b;
+c=a+b;
+cout<<" addition: "<<c;
+d=a*b;
+cout<<" multiplication: "<<d;
+e=a/b;
+cout<<" division: "<<e;
+f=a%b;
+cout<<" remainder: "<<f;
+g=a-b;
+cout<<" subtraction: "<<g;
+}
