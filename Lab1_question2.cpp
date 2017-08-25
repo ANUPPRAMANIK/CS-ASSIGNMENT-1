2accc71d3a3cff08cf53e817437db3f7a3e9cf46
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-25 14:13:33 +0530

    Update and rename README.md to Lab1_question2.cpp

---
+++ Lab1_question2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main() {
+int a,b;
+cout<<"enter an integer";
+cin>>a;
+cout<<"enter another integer";
+cin>>b;
+cout<<"sum of the two integers:"<<a+b;
+}
