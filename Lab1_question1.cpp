9cf380874967c7172e4032daf2ae33b2505f6e42
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-25 14:05:39 +0530

    Create Lab1_question1.cpp

---
+++ Lab1_question1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+main() {
+int a;
+cout<<"enter a number";
+cin>>a;
+cout<<"your number was:"<<a;
+float b;
+cout<<"enter your height in meters";
+cin>>b;
+cout<<"your height is:"<<b;
+char c[20];
+cout<<"enter your name";
+cin>>c;
+cout<<"your name is:"<<c;
+bool d;
+cout<<"are you a boy?";
+cin>>d;
+}
